90d993e27f451bb89eef07b9c2087ca89c90104c
Vivek241992 <vivekmishra.dv@gmail.com>
2018-09-29 21:09:32 +0530

    A

--- File1.R
+++ File1.R
@@ -1,1 +1,4 @@
 women
+C:/Program Files/Git/bin/git.exe commit -F C:/Users/Vivek/AppData/Local/Temp/RtmpQLPY0m/git-commit-message-2074614825c6.txt
+git config --global user.email "vivekmishra.dv@gmail.com"
+git config --global user.name "Vivek241992"
